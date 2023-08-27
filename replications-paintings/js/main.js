b7aef209e2f687d34b5c6d49e9a993c3adf6bd14
Denis <den24.03@mail.ru>
2023-08-27 14:49:55 +0500

    27.08.2023 mobile art-team end

---
+++ replications-paintings/js/main.js
@@ -0,0 +1,10 @@
+const mediaMobile400 = window.matchMedia("(max-width: 400px)");
+const products = document.querySelector(".products");
+const productsTitle = document.querySelector(".products__header-title");
+const productsList = document.querySelector(".products__list");
+
+if (mediaMobile400.matches) {
+  productsTitle.classList.add("center");
+  productsList.classList.add("center");
+  products.classList.remove("center");
+}
