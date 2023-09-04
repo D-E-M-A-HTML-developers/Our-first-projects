0205e876d3519038312127a39afbfd3fbd0b482d
Artiom <mezheynikov1@gmail.com>
2023-09-04 20:08:48 +0300

    4.08.2023 js-function-create-tempmates-products

--- replications-paintings/js/product.js
+++ replications-paintings/js/product.js
@@ -6,41 +6,69 @@ const productGermany = JSON.parse(productsGermany);
 const productEngland = JSON.parse(productsEngland);
 
 const productList = document.querySelector('.products__list');
+const productsHeaderBtn = document.querySelectorAll('.products__header-btn');
+createTemplateProducts(productFrance);
 
-productFrance.forEach(({image, author, name, description, price}) => {
-    const productEl = `
-    <li class="products__item">
-    <article>
-      <a class="products__item-link" href="#">
-        <img
-          src="${image}"
-          alt="${name}"
-          height="422"
-          class="products__item-img"
-        />
-      </a>
-      <div class="products__item-description-wrp">
-        <div class="products__item-description">
-          <span class="products__item-description-author"
-            >${author}</span
-          >
-          <h3 class="products__item-description-title">${name}</h3>
-          <p class="products__item-description-txt">
-          ${description}
-          </p>
-        </div>
-        <div class="products__item-btn-wrp">
-          <span
-            class="products__item-description-price text-color-green"
-            ><span>${price}</span> руб</span
-          >
-          <button class="products__item-btn text-color-green">
-            В&nbsp;корзину
-          </button>
-        </div>
-      </div>
-    </article>
-  </li>
-    `
-    productList.insertAdjacentHTML('beforeend',productEl);
+function createTemplateProducts(product) {
+	product.forEach(({ image, author, name, description, price }) => {
+		const productEl = `
+			<li class="products__item">
+			<article>
+				<a class="products__item-link" href="#">
+					<img
+						src="${image}"
+						alt="${name}"
+						height="422"
+						class="products__item-img"
+					/>
+				</a>
+				<div class="products__item-description-wrp">
+					<div class="products__item-description">
+						<span class="products__item-description-author"
+							>${author}</span
+						>
+						<h3 class="products__item-description-title">${name}</h3>
+						<p class="products__item-description-txt">
+						${description}
+						</p>
+					</div>
+					<div class="products__item-btn-wrp">
+						<span
+							class="products__item-description-price text-color-green"
+							><span>${price}</span> руб</span
+						>
+						<button class="products__item-btn text-color-green">
+							В&nbsp;корзину
+						</button>
+					</div>
+				</div>
+			</article>
+		</li>
+			`
+		productList.insertAdjacentHTML('beforeend', productEl);
+	});
+}
+
+productsHeaderBtn.forEach(function (item) {
+	item.addEventListener('click', open);
 });
+
+function open(evt) {
+	const tabTarget = evt.currentTarget;
+	productsHeaderBtn.forEach(function (item) {
+		item.classList.remove('header-btn_active');
+	})
+	tabTarget.classList.add('header-btn_active');
+	if (tabTarget.hasAttribute("data-btn-germany")) {
+		productList.innerHTML = '';
+		createTemplateProducts(productGermany);
+	}
+	if (tabTarget.hasAttribute("data-btn-england")) {
+		productList.innerHTML = '';
+		createTemplateProducts(productEngland);
+	}
+	if (tabTarget.hasAttribute("data-btn-france")) {
+		productList.innerHTML = '';
+		createTemplateProducts(productFrance);
+	}
+}
